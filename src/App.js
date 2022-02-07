26827da0d9b50de10ae478f75f574e7b8ccb393e
Tin Nguyen <tinnguyenhuulequang@gmail.com>
2022-02-08 08:25:06 +0900

    show task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import Header from "./Components/Header";
+import Tasks from "./Components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctor Appointment",
+      day: "Feb 5th at 3:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Teacher Appointment",
+      day: "Mar 5th at 5:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Boyfriend Appointment",
+      day: "May 5th at 9:30pm",
+      reminder: false,
+    },
+  ]);
+  //Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+  
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No task to show"
+      )}
     </div>
   );
 }
--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,5 +1,21 @@
-const Button = () => {
-  return <button className="btn">Add</button>;
+import PropTypes from 'prop-types';
+
+const Button = ({ color, text,onClick }) => {
+  
+  return (
+    <button onClick={onClick} style={{ backgroundColor: color }} className="btn">
+      {text}
+    </button>
+  );
 };
 
+Button.defaultProp={
+    color:"gray"
+}
+
+Button.prototype={
+    text:PropTypes.string,
+    color:PropTypes.string,
+    onClick:PropTypes.func,
+}
 export default Button;
