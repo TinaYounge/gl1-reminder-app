8446be53196186ca4a72c958fd266fffacdd8b9e
Tin Nguyen <tinnguyenhuulequang@gmail.com>
2022-02-08 08:57:24 +0900

    add the show addtask

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import AddTask from "./Components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask]=useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -45,9 +46,9 @@ setTasks([...tasks,newTask]) }
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask}/>
-
+      <Header onAdd ={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+{    showAddTask &&  <AddTask onAdd={addTask}/>
+}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
